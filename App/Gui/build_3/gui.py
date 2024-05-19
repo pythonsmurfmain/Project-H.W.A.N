7a1c16c4d5b75f66d94b8b1d70b3d090cc93495a
pythonsmurfmain <141853683+pythonsmurfmain@users.noreply.github.com>
2024-05-19 15:54:47 +0530

    Update gui.py

--- App/Gui/build_3/gui.py
+++ App/Gui/build_3/gui.py
@@ -3,7 +3,7 @@ from tkinter import Tk, Canvas, Entry, Text, Button, PhotoImage
 
 
 OUTPUT_PATH = Path(__file__).parent
-ASSETS_PATH = OUTPUT_PATH / Path(r"D:\HWAN\Gui\build_3\assets\frame0")
+ASSETS_PATH = OUTPUT_PATH / Path(r"Gui\build_3\assets\frame0")
 
 
 def relative_to_assets(path: str) -> Path:
